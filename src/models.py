52df7ee3674d25cd5dc85bef638fc077c9fa527a
Ramiro Scarinci <151871077+RamiroSca@users.noreply.github.com>
2024-03-17 12:17:44 +0000

    Comenzando la base de datos de SWAPI

--- src/models.py
+++ src/models.py
@@ -14,6 +14,19 @@ class Person(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
 
+
+
+class Vehiculos(Base):
+    __tablename__ = 'vehiculos'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    modelo=Column(String(100))
+
+
+
+
 class Address(Base):
     __tablename__ = 'address'
     # Here we define columns for the table address.
